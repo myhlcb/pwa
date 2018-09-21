708cf7a8cc8c53e86935c0d1e4caeb7db0def315
myhlcb <1029785965@qq.com>
2018-09-21 14:36:48 +0800

    what 【add】add pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { environment } from "../environments/environment";
+import {
+  MatToolbarModule,
+  MatCardModule,
+  MatButtonModule
+} from "@angular/material";
+import { ImgCardComponent } from "./img-card/img-card.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, ImgCardComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    ServiceWorkerModule.register("ngsw-worker.js", {
+      enabled: environment.production
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/img-card/img-card.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-img-card",
+  templateUrl: "./img-card.component.html",
+  styleUrls: ["./img-card.component.css"]
+})
+export class ImgCardComponent implements OnInit {
+  private image: CatImage = {
+    message: "Progressive Web Cat",
+    api: "https://cataas.com/cat/says/",
+    fontsize: 40
+  };
+  public src: string;
+  disabled = false;
+  ngOnInit() {
+    if (navigator.onLine) {
+      this.generateSrc();
+    } else {
+      this.disabled = true;
+      this.src = "assets/offline.jepg";
+    }
+  }
+  generateSrc(): void {
+    this.src =
+      this.image.api +
+      this.image.message +
+      "?size=" +
+      this.image.fontsize +
+      "&ts=" +
+      Date.now();
+  }
+  constructor() {}
+}
+class CatImage {
+  message: string;
+  api: string;
+  fontsize: number;
+}
